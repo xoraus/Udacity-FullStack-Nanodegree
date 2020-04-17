fbc7482de1ccdd56737975a324fc567b8f08821a
xoraus <imsafay@gmail.com>
2020-04-17 20:05:49 +0530

    flask-app-ready

---
+++ app.py
@@ -0,0 +1,34 @@
+import json
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sajjadsalaria@localhost:5432/todo'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+      __tablename__ = 'todos'
+      id = db.Column(db.Integer, primary_key=True)
+      description = db.Column(db.String(), nullable=False)
+
+      def __repr__(self):
+            return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  description = request.get_json()['description']
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return jsonify({
+    'description': todo.description
+  })
+
+
+@app.route('/')
+def index():
+  return render_template('index.html', data=Todo.query.all())
---
+++ flask-hello-app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sajjadsalaria@localhost:5432/example'
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+  __tablename__ = 'persons'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
+def __repr__(self):
+    return f'<Person ID: {self.id}, name: {self.name}>'
+
+db.create_all()
+
+@app.route('/')
+def index():
+  person = Person.query.first()
+  return 'Hello ' + person.name
+
